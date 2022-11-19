869a02fc4e175cf185406848783e66ee0d3fa088
FatGum <51463346+kollermartin@users.noreply.github.com>
2022-11-19 20:35:38 +0100

    Add spinner (#10)
    * Add spinner
    
    * Remove unused test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
 
-  assetsPath: './assets/i18n/',
+  assetsPath: 'assets/i18n/',
   assetsPartyLogosPath: 'assets/party-logos/',
 
   apiUrl: 'https://virtserver.swaggerhub.com/kollermartin/politicz/1.0.0',
