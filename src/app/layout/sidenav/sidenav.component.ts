59ef57ee0aabe14ee152fc4bdfd58f50198d9b98
Martin Koller <kollermartin00@gmail.com>
2022-11-26 21:49:51 +0100

    sidenav responsivnes

--- src/app/layout/sidenav/sidenav.component.ts
+++ src/app/layout/sidenav/sidenav.component.ts
@@ -32,7 +32,7 @@ export class SidenavComponent implements OnInit, OnDestroy, AfterViewInit {
     private router: Router,
     private store: Store
   ) {
-    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQuery = media.matchMedia('(max-width: 1200px)');
     this._mobileQueryListener = () => changeDetectorRef.detectChanges();
     this.mobileQuery.addEventListener('change', this._mobileQueryListener);
   }
