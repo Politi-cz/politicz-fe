865eab76afc49e9f7bf8150e39dc9091f0fd2f8e
FatGum <51463346+kollermartin@users.noreply.github.com>
2022-12-20 16:35:13 +0100

    Update to Angular 15 (#16)
    * jest
    
    * angular v15
    
    * Angular material v15
    
    * update jobs

--- src/app/shared/service/notification.service.ts
+++ src/app/shared/service/notification.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
-import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
+import { MatLegacySnackBar as MatSnackBar, MatLegacySnackBarHorizontalPosition as MatSnackBarHorizontalPosition, MatLegacySnackBarVerticalPosition as MatSnackBarVerticalPosition } from '@angular/material/legacy-snack-bar';
 
 @Injectable({
   providedIn: 'root',
