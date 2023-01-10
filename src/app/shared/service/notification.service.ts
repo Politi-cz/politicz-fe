de59ef05255ed576d592c0d43224ff07e0008399
FatGum <51463346+kollermartin@users.noreply.github.com>
2023-01-10 17:50:46 +0100

    Full migration to angular and material V15 (#19)
    * Fix GH actions, migrate legacy components, fix peer dependencies
    
    * Fix sidenav item for political party
    
    * Fix media at politician card
    
    * Fix font sizes

--- src/app/shared/service/notification.service.ts
+++ src/app/shared/service/notification.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, NgZone } from '@angular/core';
-import { MatLegacySnackBar as MatSnackBar, MatLegacySnackBarHorizontalPosition as MatSnackBarHorizontalPosition, MatLegacySnackBarVerticalPosition as MatSnackBarVerticalPosition } from '@angular/material/legacy-snack-bar';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
