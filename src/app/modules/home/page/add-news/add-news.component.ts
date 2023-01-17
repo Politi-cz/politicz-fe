9ad50ce8a94bf397a3564e32d2bf1de9d965c152
Martin Koller <kollermartin00@gmail.com>
2023-01-17 18:26:12 +0100

    Wysiwig editor

--- src/app/modules/home/page/add-news/add-news.component.ts
+++ src/app/modules/home/page/add-news/add-news.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-add-news',
   templateUrl: './add-news.component.html',
-  styleUrls: ['./add-news.component.scss']
+  styleUrls: ['./add-news.component.scss'],
 })
-export class AddNewsComponent {
+export class AddNewsComponent implements OnInit {
+  newsForm: FormGroup;
+  public readonly config = {
+    editable: true,
+    height: 'auto',
+    minHeight: '25rem',
+    maxHeight: 'auto',
+    enableToolbar: false,
+    showToolbar: true,
+  } as AngularEditorConfig;
 
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.newsForm = this._fb.group({
+      heading: ['', Validators.required],
+      content: [''],
+    });
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,20 @@ import { RouterModule } from '@angular/router';
 import { MaterialModule } from './material.module';
 import { PipeModule } from './pipe/pipe.module';
 import { AdminQuickMenuActionsComponent } from './component/admin-quick-menu-actions/admin-quick-menu-actions.component';
+import { AngularEditorModule } from '@kolkov/angular-editor';
 
 @NgModule({
   declarations: [AdminQuickMenuActionsComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterModule, TranslateModule, PipeModule, MaterialModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    TranslateModule,
+    PipeModule,
+    MaterialModule,
+    AngularEditorModule,
+  ],
   exports: [
     CommonModule,
     FormsModule,
@@ -19,6 +29,7 @@ import { AdminQuickMenuActionsComponent } from './component/admin-quick-menu-act
     TranslateModule,
     PipeModule,
     AdminQuickMenuActionsComponent,
+    AngularEditorModule,
   ],
 })
 export class SharedModule {}
