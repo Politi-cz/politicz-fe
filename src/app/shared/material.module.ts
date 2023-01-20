413852fb5a63edf56b34000dc7d7b2e5c5a742b9
Martin Koller <kollermartin00@gmail.com>
2023-01-20 21:02:44 +0100

    Add-politician form

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -9,7 +9,7 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatRippleModule } from '@angular/material/core';
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -68,6 +68,7 @@ const materialModules = [
   MatDatepickerModule,
   MatTooltipModule,
   MatSlideToggleModule,
+  MatNativeDateModule,
 ];
 
 @NgModule({
