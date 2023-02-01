b3afd0bab0b673bb24b2ebd4c34c5a5ca913a97a
Martin Koller <kollermartin00@gmail.com>
2023-02-01 20:19:20 +0100

    Make abstractFormComponent, start creating partyForm

--- src/app/modules/party/forms/party-form/party-form.component.ts
+++ src/app/modules/party/forms/party-form/party-form.component.ts
@@ -1,11 +1,33 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { IPoliticalParty } from './../../../../data/schema/political-party';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AbstractFormComponent } from '../../../../shared/forms/abstractForm';
 
 @Component({
   selector: 'app-party-form',
   templateUrl: './party-form.component.html',
   styleUrls: ['./party-form.component.scss'],
 })
-export class PartyFormComponent {
-  @Input() party: IPoliticalParty | undefined;
+export class PartyFormComponent extends AbstractFormComponent implements OnInit {
+  @Input() party: IPoliticalParty | null;
+
+  public partyForm: FormGroup = this._fb.group({
+    name: this._fb.control('', Validators.required),
+    image: this._fb.control('', Validators.required),
+    tagsControl: this._fb.control(''),
+    politicians: this._fb.control(''),
+  });
+
+  constructor(private _fb: FormBuilder) {
+    super();
+  }
+
+  ngOnInit(): void {
+    if (this.party) {
+      this.partyForm.patchValue({
+        name: this.party.name,
+        image: this.party.image,
+      });
+    }
+  }
 }
