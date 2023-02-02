0d73e0d627c226fee9596dbeb5ab8d80fec67b83
Martin Koller <kollermartin00@gmail.com>
2023-02-02 20:13:06 +0100

    Make form nonullable

--- src/app/modules/party/forms/party-form/party-form.component.ts
+++ src/app/modules/party/forms/party-form/party-form.component.ts
@@ -1,4 +1,4 @@
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { IPoliticalParty } from './../../../../data/schema/political-party';
 import { Component, Input, OnInit } from '@angular/core';
 import { AbstractFormComponent } from '../../../../shared/forms/abstractForm';
@@ -11,7 +11,7 @@ import { AbstractFormComponent } from '../../../../shared/forms/abstractForm';
 export class PartyFormComponent extends AbstractFormComponent implements OnInit {
   @Input() party: IPoliticalParty | null;
 
-  public partyForm: FormGroup = this._fb.group({
+  public partyForm = this._fb.nonNullable.group({
     name: this._fb.control('', Validators.required),
     image: this._fb.control('', Validators.required),
     tagsControl: this._fb.control(''),
@@ -24,10 +24,6 @@ export class PartyFormComponent extends AbstractFormComponent implements OnInit 
 
   ngOnInit(): void {
     if (this.party) {
-      this.partyForm.patchValue({
-        name: this.party.name,
-        image: this.party.image,
-      });
     }
   }
 }
