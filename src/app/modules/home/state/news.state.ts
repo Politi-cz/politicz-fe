7e51fbc742f68653ba2a91d34b5cbe064035238b
Martin Koller <kollermartin00@gmail.com>
2023-02-28 18:29:29 +0100

    Refactor news state

--- src/app/modules/home/state/news.state.ts
+++ src/app/modules/home/state/news.state.ts
@@ -1,3 +1,4 @@
+import { tap, Observable } from 'rxjs';
 import { NewsService } from '../../../data/service/news.service';
 import { INews } from '../../../data/schema/news';
 import { Action, State, StateContext } from '@ngxs/store';
@@ -13,18 +14,18 @@ export class NewsState {
   //TODO refactor, return observable instead of subscribe
   constructor(private newsService: NewsService) {}
 
-  @Action(News.GetAll) public getNews(ctx: StateContext<INews[]>): void {
-    this.newsService.getAllNews().subscribe((news: INews[]) => ctx.setState(news));
+  @Action(News.GetAll) public getNews(ctx: StateContext<INews[]>): Observable<INews[]> {
+    return this.newsService.getAllNews().pipe(tap((news: INews[]) => ctx.setState(news)));
   }
 
   @Action(News.Remove) public removeNews(
     ctx: StateContext<INews[]>,
     { payload }: News.Remove,
-  ): void {
-    this.newsService
+  ): Observable<void> {
+    return this.newsService
       .deleteNews(payload.id)
-      .subscribe(() =>
-        ctx.setState(ctx.getState().filter((news: INews) => news.id !== payload.id)),
+      .pipe(
+        tap(() => ctx.setState(ctx.getState().filter((news: INews) => news.id !== payload.id))),
       );
   }
 }
