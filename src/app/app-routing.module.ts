fc39b9071311b6b4911ea72fde1d8afab9307a68
Martin Koller <kollermartin00@gmail.com>
2023-04-02 16:18:50 +0200

    Simplify routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,21 @@
 import { AdminSectionComponent } from './shared/component/admin-section/admin-section.component';
-import { ContentLayoutComponent } from './layout/content-layout/content-layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './core/guard/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/news', pathMatch: 'full' },
   {
-    path: '',
-    component: ContentLayoutComponent,
-    canActivate: [AuthGuard],
-    children: [
-      {
-        path: 'news',
-        loadChildren: () => import('./modules/home/home.module').then((m: any) => m.HomeModule),
-      },
-      {
-        path: 'political-party',
-        loadChildren: () => import('./modules/party/party.module').then((m: any) => m.PartyModule),
-      },
-      {
-        path: 'admin',
-        component: AdminSectionComponent,
-      },
-    ],
+    path: 'news',
+    loadChildren: () => import('./modules/home/home.module').then((m: any) => m.HomeModule),
   },
+  {
+    path: 'political-party',
+    loadChildren: () => import('./modules/party/party.module').then((m: any) => m.PartyModule),
+  },
+  {
+    path: 'admin',
+    component: AdminSectionComponent,
+  },
+  { path: '', redirectTo: '/news', pathMatch: 'full' },
   { path: '**', redirectTo: '/news', pathMatch: 'full' },
 ];
 
