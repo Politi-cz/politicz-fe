f57e0ca235f12af4e237be693bb8f07290cb2ede
Martin Koller <kollermartin00@gmail.com>
2023-04-16 14:26:36 +0200

    Add auth config for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,4 +5,13 @@ export const environment = {
   assetsPartyLogosPath: 'assets/party-logos/',
 
   apiUrl: 'https://virtserver.swaggerhub.com/kollermartin/politicz/1.0.0',
+
+  auth: {
+    domain: 'dev-zu5pzoxyf4a2amfg.us.auth0.com',
+    clientId: 'mgQUdiVLoVdejanEtTx7OHqSVk9aJ5sx',
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+      audience: 'https://politi.cz',
+    },
+  },
 };
