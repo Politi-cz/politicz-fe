da9040a2d356a8f665ccc69e53ca73383db59ab9
Martin Koller <kollermartin00@gmail.com>
2023-05-08 21:25:11 +0200

    Improve search input behavior

--- src/app/modules/party/page/party.component.ts
+++ src/app/modules/party/page/party.component.ts
@@ -1,7 +1,7 @@
 import { IPolitician } from '../../../data/schema/politician';
 import { PoliticalPartyState } from '../state/political-party.state';
 import { FiltersState } from '../../../state/filters.state';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Select, Store } from '@ngxs/store';
 import { map, Observable } from 'rxjs';
@@ -13,6 +13,7 @@ import { Permission } from '../../../data/schema/permission.enum';
 import { Utils } from '../../../shared/utils/utils';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { MatInput } from '@angular/material/input';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-party',
@@ -38,7 +39,7 @@ import { MatInput } from '@angular/material/input';
     ]),
   ],
 })
-export class PartyComponent implements OnInit {
+export class PartyComponent implements OnInit, OnDestroy {
   @Select(FiltersState.getPoliticianFilterCount) politiciansCount: Observable<number>;
 
   @Select(PoliticalPartyState) politicalParty$: Observable<IPoliticalParty>;
@@ -55,6 +56,8 @@ export class PartyComponent implements OnInit {
 
   public searchState: string = 'closed';
 
+  public searchControl = new FormControl('', { nonNullable: true });
+
   constructor(
     private _route: ActivatedRoute,
     private _store: Store,
@@ -64,11 +67,19 @@ export class PartyComponent implements OnInit {
 
   public ngOnInit(): void {
     this._route.paramMap.subscribe((params: ParamMap) => {
+      this.searchControl.reset();
+      this.searchState = 'closed';
+
       return this._store.dispatch(new PoliticalParty.GetPoliticalParty(params.get('id')));
     });
   }
 
+  public ngOnDestroy(): void {
+    this.searchControl.reset();
+  }
+
   public toggleSearch(): void {
     this.searchState = this.searchState === 'closed' ? 'open' : 'closed';
+    this.searchControl.reset();
   }
 }
