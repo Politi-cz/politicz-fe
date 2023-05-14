c2659c635424fc0c8c3f35191f14f950265bf41e
Martin Koller <kollermartin00@gmail.com>
2023-05-14 17:57:26 +0200

    remove redunant config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
