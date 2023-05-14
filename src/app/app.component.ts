f44e1cae4deb6749e0afed29a935a88f5fedba0e
Martin Koller <kollermartin00@gmail.com>
2023-05-14 16:48:53 +0200

    Add bug fix for devices scroll
    (cherry picked from commit 7dbb7adeb9ed3e56ad7ad1dfa239f4078415d0af)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private _router: Router) {}
+
+  public ngOnInit(): void {
+    //Workaround for bug on mobile devices that you are not scrolled up when changing route
+    this._router.events.subscribe((event: any) => {
+      if (!(event instanceof NavigationEnd)) {
+        return;
+      }
+      document.body.scrollTop = 0;
+    });
+  }
+}
